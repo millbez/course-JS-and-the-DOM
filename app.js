32f1fd180a7152fba4bb08818c494d7f92c290a6
millbez <millionbezabihg@gmail.com>
2025-05-05 17:21:43 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('click', function() {
+    // select the element
+    const mainHeading = document.querySelector('h1');
+    
+    // change its background color to red
+  	mainHeading.style.backgroundColor = 'red';
+})
