faf85f9e93d50682b3a076a5441a0549dbf80329
millbez <millionbezabihg@gmail.com>
2025-05-06 19:10:20 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+<script>
+    document.addEventListener('DOMContentLoaded', e => {
+        // select the h1 by a class, id, or even tag name
+        // in this example, i'm just selecting it by tag name
+        const h1 = document.querySelector("h1")
+
+        h1.innerText = "Correct Text"
+    })
+</script>
