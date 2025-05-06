9730b8f761bf89cbec6e87a19d969ac732d08efe
millbez <millionbezabihg@gmail.com>
2025-05-06 18:47:07 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+content="width=device-width,
+    <title>Change H1 Text</t
+</head>
+<body>
+    <h1 id="myheader">origin
+    <button
+id="changeTextButton">Change
+      <script>
+  document.getElementById('c
+    
+  document.getElementById('m
+  });
+      </script>
+        </body>
+        </html>
+    
