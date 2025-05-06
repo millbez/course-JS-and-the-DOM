96127baea44faf7279506d1788070680ac367db4
millbez <millionbezabihg@gmail.com>
2025-05-06 19:13:25 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+<script>
+ document.getElementById('change TextButton').addEventListener('click', function(){
+   
+ document.getElementById('myHeader').textContent='Build Anar App';
+ });
+     </script>
